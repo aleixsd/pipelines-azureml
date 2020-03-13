56c232b1fceb826f70f608550220ca9411459b42
aleixsd <61747168+aleixsd@users.noreply.github.com>
2020-03-13 17:44:49 +0100

    Update score.py

--- models/diabetes/score.py
+++ models/diabetes/score.py
@@ -1,22 +1,16 @@
 import json
-import numpy as np
-import pickle
-from sklearn.linear_model import Ridge
+import numpy
 from azureml.core.model import Model
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from sklearn.externals import joblib
 
+
+###############################
 def init():
     global model
-    model_path = Model.get_model_path('diabetes-model')
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
+    model_path = Model.get_model_path(model_name = 'sklearn_regression_model')
+    model = joblib.load(model_path)
+	
 
-input_sample = np.array([[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]])
-output_sample = np.array([3726.995])
-
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
 def run(data):
     try:
         result = model.predict(data)
@@ -25,3 +19,4 @@ def run(data):
     except Exception as e:
         error = str(e)
         return error
+
