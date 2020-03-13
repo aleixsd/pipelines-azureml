2a927c8dad31f4505119b4bb1052814e4b57238b
aleixsd <61747168+aleixsd@users.noreply.github.com>
2020-03-13 17:24:53 +0100

    Update train.py

--- models/diabetes/train.py
+++ models/diabetes/train.py
@@ -1,44 +1,40 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
-
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from azureml.core.run import Run
-import pickle
+from sklearn.externals import joblib
+import azureml.core
+from azureml.core import Workspace, Experiment, ScriptRunConfig
+from azureml.core.environment import Environment
+import pandas as pd
 import os
-import numpy as np
-import mylib
 
-os.makedirs('./outputs', exist_ok=True)
+from azureml.core.run import Run
 
-X, y = load_diabetes(return_X_y=True)
+import sklearn
 
 run = Run.get_context()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    random_state=0)
+X, y = load_diabetes(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 data = {"train": {"X": X_train, "y": y_train},
         "test": {"X": X_test, "y": y_test}}
+		
 
-# list of numbers from 0.0 to 1.0 with a 0.05 interval
-alphas = mylib.get_alphas()
+alpha = 0.5
+run.log("alpha_value", alpha)
 
-for alpha in alphas:
-    # Use Ridge algorithm to create a regression model
-    reg = Ridge(alpha=alpha)
-    reg.fit(data["train"]["X"], data["train"]["y"])
+reg = Ridge(alpha=alpha)
+reg.fit(data["train"]["X"], data["train"]["y"])
 
-    preds = reg.predict(data["test"]["X"])
-    mse = mean_squared_error(preds, data["test"]["y"])
-    run.log('alpha', alpha)
-    run.log('mse', mse)
+preds = reg.predict(data["test"]["X"])
 
-    # save model in the outputs folder so it automatically get uploaded
-    model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
-    with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
-        pickle.dump(reg, file)
+mse = mean_squared_error(preds, y_test)
+run.log("mse", mse)
 
-    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
+model_name = "sklearn_regression_model.pkl"
+
+joblib.dump(value=reg, filename="./outputs/" + model_name)
+
+#run.upload_file(name=model_name, path_or_stream="outputs/" + model_name)
