92f16716db2e83dd0262e8ed5473217937142c24
aleixsd <61747168+aleixsd@users.noreply.github.com>
2020-03-16 13:21:01 +0100

    Update score.py

--- models/diabetes/score.py
+++ models/diabetes/score.py
@@ -9,12 +9,11 @@ def init():
     global model
     model_path = Model.get_model_path(model_name = 'sklearn_regression_model')
     model = joblib.load(model_path)
-	
 
-def run(raw_data):
+def run(input_data):
     try:
         
-	data = json.loads(raw_data)["data"]
+	data = json.loads(input_data)["data"]
 	data = numpy.array(data)
 	result = model.predict(data)
         # you can return any datatype as long as it is JSON-serializable
