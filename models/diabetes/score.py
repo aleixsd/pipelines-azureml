9f3779ffe572c034ca164727cf972e9e683b1840
aleixsd <61747168+aleixsd@users.noreply.github.com>
2020-03-16 13:34:36 +0100

    Update score.py

--- models/diabetes/score.py
+++ models/diabetes/score.py
@@ -12,13 +12,11 @@ def init():
 
 def run(input_data):
     try:
-        
-	data = json.loads(input_data)["data"]
-	data = numpy.array(data)
-	result = model.predict(data)
+        data = json.loads(input_data)['data']
+        data = numpy.array(data)
+        result = model.predict(data)
         # you can return any datatype as long as it is JSON-serializable
         return result.tolist()
     except Exception as e:
         error = str(e)
         return error
-
